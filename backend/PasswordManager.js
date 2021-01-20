406f6b25d0b43d2ee0b98e1cff3fe2c5672afa7d
bsaikiran618 <bsaikiran618@gmail.com>
2021-01-20 19:34:08 +0530

    Added PasswordManager.js and authManager.js

---
+++ backend/PasswordManager.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+router.post("/generatePassword", (req, res)=>{
+	
+})
+
+router.post("/updatePassword", (req, res)=>{
+	
+})
+
+router.post("/deletePassword", (req, res)=>{
+	
+})
+
+module.exports = router
